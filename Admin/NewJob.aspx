f4fc8229eda2303d7673b155b2a0fa86cbf993a8
MRibahna <121235590+MRibahna@users.noreply.github.com>
2025-03-16 00:12:38 +0600

    Add files via upload

---
+++ Admin/NewJob.aspx
@@ -0,0 +1,132 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/AdminMaster.Master" AutoEventWireup="true" CodeBehind="NewJob.aspx.cs" Inherits="OnlineJobPortal.Admin.newJobs" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div style="background-image: url('../Image/bg.jpg'); width:100%; height: 720px; background-repeat: no-repeat; background-size:cover ; background-attachment: fixed;">
+        <div class="container pt-4 pb-4">
+            
+            <div class="btn-toolbar justify-content-between mb-3">
+                <div class="btn-group">
+                        <asp:Label ID="lblMsg" runat="server"></asp:Label>
+                </div>
+                <div class="input-group h-25">
+                    <asp:HyperLink ID="linkBack" runat="server" NavigateUrl="~/Admin/JobList.aspx" CssClass="btn btn-secondary" Visible="false">
+                        < Back </asp:HyperLink>
+                </div>
+            </div>
+        
+        <h3 class="text-center"><%Response.Write(Session["title"]); %></h3>
+        <div class="row mr-lg-5 ml-lg-5 pb-3">
+            <div class="col-md-6 pt-3">
+                <label for="txtJobTitle" style = "font-weight:600 ">Job Title</label>
+                <asp:TextBox ID="txtJobTitle" runat="server" CssClass="form-control" placeHolder="Ex. Web Developer, App Developer" required></asp:TextBox>
+            </div>
+             <div class="col-md-6 pt-3">
+                <label for="txtNoofPost" style = "font-weight:600 ">Number of posts</label>
+                <asp:TextBox ID="txtNoofPost" runat="server" CssClass="form-control" placeHolder="Enter Number of Vacant Positions" required TextMode="Number"></asp:TextBox>
+            </div>
+            
+        </div>
+
+            <div class="row mr-lg-5 ml-lg-5 pb-3">
+            <div class="col-md-12 pt-3">
+                <label for="txtDescription" style = "font-weight:600 ">Description</label>
+                <asp:TextBox ID="txtDescription" runat="server" CssClass="form-control" placeHolder="Enter Job Description" TextMode="MultiLine" required></asp:TextBox>
+            </div>
+            
+            
+        </div>
+
+            <div class="row mr-lg-5 ml-lg-5 pb-3">
+            <div class="col-md-6 pt-3">
+                <label for="txtQualification" style = "font-weight:600 ">Required Qualification/Education </label>
+                <asp:TextBox ID="txtQualification" required runat="server" CssClass="form-control" placeHolder="Ex. BSC, BSC in Engineering, MBA"></asp:TextBox>
+            </div>
+             <div class="col-md-6 pt-3">
+                <label for="txtExperience" style = "font-weight:600 ">Required Experience</label>
+                <asp:TextBox ID="txtExperience" required runat="server" CssClass="form-control" placeHolder="Ex. 2 Years, 1 year" ></asp:TextBox>
+            </div>
+            
+        </div>
+
+            <div class="row mr-lg-5 ml-lg-5 pb-3">
+            <div class="col-md-6 pt-3">
+                <label for="txtSpecialization" style = "font-weight:600 ">Required Specialization</label>
+                <asp:TextBox ID="txtSpecialization" required runat="server" CssClass="form-control" placeHolder="Enter Specialization" TextMode="MultiLine"></asp:TextBox>
+            </div>
+             <div class="col-md-6 pt-3">
+                <label for="txtDeadline" style = "font-weight:600 ">Application Deadline</label>
+                <asp:TextBox ID="txtDeadline" required runat="server" CssClass="form-control" placeHolder="Enter Application Deadline" TextMode="Date" ></asp:TextBox>
+            </div>
+            
+        </div>
+
+            <div class="row mr-lg-5 ml-lg-5 pb-3">
+            <div class="col-md-6 pt-3">
+                <label for="txtSalary" style = "font-weight:600 ">Salary</label>
+                <asp:TextBox ID="txtSalary" required runat="server" CssClass="form-control" placeHolder="Enter Monthly Salary or Annual Salary" ></asp:TextBox>
+            </div>
+             <div class="col-md-6 pt-3">
+                <label for="ddlJobType" style = "font-weight:600 ">Job Type</label>
+                 <asp:DropDownList ID="ddlJobType" runat="server" CssClass="form-control">
+                     <asp:ListItem Value="0">Select Job Type</asp:ListItem>
+                     <asp:ListItem>Full Time</asp:ListItem>
+                     <asp:ListItem>Part Time</asp:ListItem>
+                     <asp:ListItem>Remote</asp:ListItem>
+                     <asp:ListItem>Freelance</asp:ListItem>
+                 </asp:DropDownList>
+                 <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="Jobtype is required" ForeColor="Red" ControlToValidate="ddlJobType" InitialValue="0" Display="Dynamic" SetFocusOnError="true">
+
+                 </asp:RequiredFieldValidator>
+            </div>
+            
+        </div>
+
+            <div class="row mr-lg-5 ml-lg-5 pb-3">
+            <div class="col-md-6 pt-3">
+                <label for="txtCompany" style = "font-weight:600 ">Company/Organization Name</label>
+                <asp:TextBox ID="txtCompany" required runat="server" CssClass="form-control" placeHolder="Enter Company/Organization Name" ></asp:TextBox>
+            </div>
+             <div class="col-md-6 pt-3">
+                <label for="ddlJobType" style = "font-weight:600 ">Company/Organization logo</label>
+                 <asp:FileUpload ID="fuCompanyLogo" runat="server" CssClass="form-control" ToolTip=".jpg, .jpeg, .png extension only" />
+                 
+            </div>
+            
+        </div>
+
+            <div class="row mr-lg-5 ml-lg-5 pb-3">
+            <div class="col-md-6 pt-3">
+                <label for="txtWebsite" style = "font-weight:600 ">Website </label>
+                <asp:TextBox ID="txtWebsite" runat="server" CssClass="form-control" placeHolder="Enter Website" TextMode="Url"></asp:TextBox>
+            </div>
+             <div class="col-md-6 pt-3">
+                <label for="txtEmail" style = "font-weight:600 ">Official Email of the Company/Organization</label>
+                <asp:TextBox ID="txtEmail"  runat="server" CssClass="form-control" placeHolder="Enter Email" TextMode="Email" ></asp:TextBox>
+            </div>
+            
+        </div>
+
+            <div class="row mr-lg-5 ml-lg-5 pb-3">
+            <div class="col-md-12 pt-3">
+                <label for="txtAddress" style = "font-weight:600 ">Address</label>
+                <asp:TextBox ID="txtAddress" runat="server" CssClass="form-control" placeHolder="Enter Address" TextMode="MultiLine" required></asp:TextBox>
+            </div>
+            
+            
+        </div>
+
+            <div class="row mr-lg-5 ml-lg-5 mb-3 pt-4">
+                <div class="'col-md-3 col-md-offset-2 mb-3">
+                    <asp:Button ID="btnAdd" runat="server" Text="Add Job" CssClass="btn btn-primary btn-block" BackColor="#7800cf" OnClick="btnAdd_Click" />
+
+                </div>
+            
+        </div>
+
+      </div>
+
+    </div>
+
+</asp:Content>
---
+++ Admin/NewJob.aspx.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineJobPortal.Admin
+{
+    public partial class newJobs : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        string str = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        string query;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["admin"] == null)
+            {
+                Response.Redirect("~/User/Login.aspx");
+            }
+            Session["title"] = "Add Job";
+            if (!IsPostBack)
+            {
+                fillData();
+            }
+        }
+
+        private void fillData()
+        {
+            if (Request.QueryString["id"] != null)
+            {
+                con = new SqlConnection(str);
+                query = "Select * from Jobs where jobID = ' " + Request.QueryString["id"] + "' ";
+                cmd = new SqlCommand(query, con);
+                con.Open();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                if (sdr.HasRows) 
+                { 
+                    while(sdr.Read())
+                    {
+                        txtJobTitle.Text = sdr["Title"].ToString();
+                        txtNoofPost.Text = sdr["NoOfPost"].ToString();
+                        txtDescription.Text = sdr["Descriptionn"].ToString();
+                        txtQualification.Text = sdr["Qualification"].ToString();
+                        txtExperience.Text = sdr["Experience"].ToString();
+                        txtSpecialization.Text = sdr["Specialization"].ToString();
+                        txtDeadline.Text = Convert.ToDateTime( sdr["Deadline"]).ToString("yyyy-MM-dd");
+                        txtSalary.Text = sdr["Salary"].ToString(); 
+                        ddlJobType.SelectedValue = sdr["jobType"].ToString();
+                        txtCompany.Text = sdr["CompanyName"].ToString();
+                        txtEmail.Text = sdr["Email"].ToString();
+                        txtWebsite.Text = sdr["Website"].ToString();
+                        txtAddress.Text = sdr["Addresss"].ToString();
+
+                        btnAdd.Text = "Update";
+
+                        linkBack.Visible = true;
+                        Session["title"] = "Edit Job";
+
+                    }
+                }
+                else
+                {
+                    lblMsg.Text = "Job not Found!!";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+
+                sdr.Close();
+                con.Close();
+            }
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string type, concatQuery, imagePath = string.Empty;
+                bool isValidToExecute = false;
+                con=new SqlConnection(str);
+
+                if(Request.QueryString["id"]!= null)
+                {
+                    if (fuCompanyLogo.HasFile)
+                    {
+                        if (Utils.IsValidExtension(fuCompanyLogo.FileName))
+                        {
+                            concatQuery = "CompanyImage = @CompanyImage,";
+
+
+                        }
+                        else
+                        {
+                            concatQuery = string.Empty;
+
+                        }
+                    }
+                    else
+                    {
+                        concatQuery= string.Empty;
+                    }
+                    query = @"Update Jobs set Title=@Title, NoOfPost=@NoOfPost, Descriptionn=@Descriptionn,
+                            Qualification=@Qualification,Experience=@Experience,Specialization=@Specialization,
+                            Deadline=@Deadline,Salary=@Salary,jobType=@jobType,CompanyName=@CompanyName, "+ concatQuery+@"
+                            Email=@Email, Website=@Website,Addresss=@Addresss where jobID = @id";
+                    type = "Updated";
+                    
+                    cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@Title", txtJobTitle.Text.Trim());
+                    cmd.Parameters.AddWithValue("@NoOfPost", txtNoofPost.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Descriptionn", txtDescription.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Qualification", txtQualification.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Experience", txtExperience.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Specialization", txtSpecialization.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Deadline", txtDeadline.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Salary", txtSalary.Text.Trim());
+                    cmd.Parameters.AddWithValue("@jobType", ddlJobType.SelectedValue);
+                    cmd.Parameters.AddWithValue("@CompanyName", txtCompany.Text.Trim());
+                    //cmd.Parameters.AddWithValue("@CompanyImage", .Text.Trim());
+                    cmd.Parameters.AddWithValue("@Email", txtEmail.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Website", txtWebsite.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Addresss", txtAddress.Text.Trim());
+                    cmd.Parameters.AddWithValue("@id", Request.QueryString["id"].ToString());
+
+                    if (fuCompanyLogo.HasFile)
+                    {
+                        if (Utils.IsValidExtension(fuCompanyLogo.FileName))
+                        {
+                            Guid obj = Guid.NewGuid();
+                            imagePath = "Images/" + obj.ToString() + fuCompanyLogo.FileName;
+                            fuCompanyLogo.PostedFile.SaveAs(Server.MapPath("~/Images/") + obj.ToString() + fuCompanyLogo.FileName);
+
+                            cmd.Parameters.AddWithValue("@CompanyImage", imagePath);
+                            isValidToExecute = true;
+                        }
+                        else
+                        {
+                            lblMsg.Text = "File should be in .jpg, .jpeg or in .ong format.";
+                            lblMsg.CssClass = "alert alert-danger";
+                        }
+                    }
+                    else
+                    {
+                        isValidToExecute = true;
+                    }
+
+                }
+                else
+                {
+                    query = @"Insert into Jobs values (@Title, @NoOfPost, @Descriptionn,@Qualification,@Experience,@Specialization,
+                            @Deadline,@Salary,@jobType,@CompanyName,@CompanyImage,@Email, @Website,@Addresss,@CreateDate)";
+                    type = "Saved";
+                    DateTime time = DateTime.Now;
+                    cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@Title", txtJobTitle.Text.Trim());
+                    cmd.Parameters.AddWithValue("@NoOfPost", txtNoofPost.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Descriptionn", txtDescription.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Qualification", txtQualification.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Experience", txtExperience.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Specialization", txtSpecialization.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Deadline", txtDeadline.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Salary", txtSalary.Text.Trim());
+                    cmd.Parameters.AddWithValue("@jobType", ddlJobType.SelectedValue);
+                    cmd.Parameters.AddWithValue("@CompanyName", txtCompany.Text.Trim());
+                    //cmd.Parameters.AddWithValue("@CompanyImage", .Text.Trim());
+                    cmd.Parameters.AddWithValue("@Email", txtEmail.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Website", txtWebsite.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Addresss", txtAddress.Text.Trim());
+                    cmd.Parameters.AddWithValue("@CreateDate", time.ToString("yyyy-MM-dd HH:mm:ss"));
+
+                    if (fuCompanyLogo.HasFile)
+                    {
+                        if (Utils.IsValidExtension(fuCompanyLogo.FileName))
+                        {
+                            Guid obj = Guid.NewGuid();
+                            imagePath = "Images/" + obj.ToString() + fuCompanyLogo.FileName;
+                            fuCompanyLogo.PostedFile.SaveAs(Server.MapPath("~/Images/") + obj.ToString() + fuCompanyLogo.FileName);
+
+                            cmd.Parameters.AddWithValue("@CompanyImage", imagePath);
+                            isValidToExecute = true;
+                        }
+                        else
+                        {
+                            lblMsg.Text = "File should be in .jpg, .jpeg or in .ong format.";
+                            lblMsg.CssClass = "alert alert-danger";
+                        }
+                    }
+                    else
+                    {
+                        cmd.Parameters.AddWithValue("@CompanyImage", imagePath);
+                        isValidToExecute = true;
+                    }
+
+                    
+                }
+                if (isValidToExecute)
+                {
+                    con.Open();
+                    int res = cmd.ExecuteNonQuery();
+                    if (res > 0)
+                    {
+                        lblMsg.Text = "Job " + type + " Successfully!!";
+                        lblMsg.CssClass = "alert alert-success";
+                        Clear();
+                    }
+                    else
+                    {
+                        lblMsg.Text = "Cannot " + type + " the records, Please try again after sometime.";
+                        lblMsg.CssClass = "alert alert-danger";
+
+                    }
+                }
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+            finally 
+            { 
+                con.Close();
+            }
+        }
+
+        private void Clear()
+        {
+            txtJobTitle.Text = string.Empty;
+            txtNoofPost.Text = string.Empty;
+            txtDescription.Text = string.Empty;
+            txtQualification.Text = string.Empty;
+            txtExperience.Text = string.Empty;
+            txtSpecialization.Text = string.Empty;
+            txtDeadline.Text = string.Empty;
+            txtSalary.Text = string.Empty;
+            ddlJobType.ClearSelection();
+            txtCompany.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+            txtWebsite.Text = string.Empty;
+            txtAddress.Text = string.Empty;
+
+        }
+
+        //private bool IsValidExtension(string fileName)
+        //{
+
+        //    bool isValid = false;
+        //    string[] fileExtension = { ".jpg", ".jpeg", ".png" };
+        //    for (int i = 0; i <= fileExtension.Length - 1; i++)
+        //    {
+        //        if (fileName.Contains(fileExtension[i]))
+        //        {
+        //            isValid = true;
+        //            break;
+        //        }
+        //    }
+        //    return isValid;
+        //}
+    }
+}
